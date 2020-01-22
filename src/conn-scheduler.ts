d3fbeeea3668fe41f2157663b554c2636fb7c2a1
Andre Staltz <andre@staltz.com>
2020-01-22 13:22:29 +0200

    update scheduler: remove neverJustOne, it was hard to justify it

--- src/conn-scheduler.ts
+++ src/conn-scheduler.ts
@@ -96,11 +96,6 @@ function shufflePeers(peers: Array<Peer>) {
   return peers.sort(() => Math.random() - 0.5);
 }
 
-function neverJustOne(x: number) {
-  if (x === 1) return x + 1;
-  else return x;
-}
-
 const minute = 60e3;
 const hour = 60 * 60e3;
 
@@ -205,7 +200,7 @@ export class ConnScheduler {
     const peersDown = query.peersConnectable('db').filter(test);
     const {quota, backoffStep, backoffMax, groupMin} = opts;
     const excess = peersUp.length > quota * 2 ? peersUp.length - quota : 0;
-    const freeSlots = neverJustOne(Math.max(quota - peersUp.length, 0));
+    const freeSlots = Math.max(quota - peersUp.length, 0);
 
     // Disconnect from excess
     peersUp
